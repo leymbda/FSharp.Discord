e1882577d45894191e153e406624ecb8b63a61cc
FinOCE <me@itsf.in>
2024-12-30 14:23:28 +1000

    Add temporary tests to empty test projects to ensure MSTest runner success

---
+++ Tests/Discordfs.Tests.Unit/Temp.fs
@@ -0,0 +1,9 @@
+﻿namespace Discordfs
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+[<TestClass>]
+type Temp () =
+    [<TestMethod>]
+    member _.``Temporary test to ensure MSTest runner success`` () =
+        Assert.IsTrue(true)
