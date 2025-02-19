0e153b15789149c6d4062b3b6893e8b923db885b
leydel <me@itsf.in>
2025-02-19 11:39:50 +1000

    Inject date provider as dependency and send events when gateway connection is active and inactive

---
+++ FSharp.Discord.Gateway/Common/CustomGatewayOpcode.fs
@@ -0,0 +1,16 @@
+﻿namespace FSharp.Discord.Gateway
+
+open FSharp.Discord.Types
+
+type CustomGatewayOpcode =
+    | ACTIVE = -1
+    | INACTIVE = -2
+
+module CustomGatewayOpcode =
+    let cast (opcode: CustomGatewayOpcode) =
+        opcode |> int |> enum<GatewayOpcode>
+
+    let toString (opcode: CustomGatewayOpcode) =
+        opcode |> int |> string
+
+// TODO: Should this be done this way? This has the potential to have a collision with discord's gateway opcodes
--- FSharp.Discord.Gateway/GatewayState.fs
+++ FSharp.Discord.Gateway/GatewayState.fs
@@ -10,6 +10,7 @@ type Model = {
     Handler: GatewayHandler
 
     // Lifecycle state
+    Active: bool
     Interval: int option
     ResumeGatewayUrl: string option
     SessionId: string option
@@ -24,32 +25,31 @@ type Model = {
 type Msg =
     | Send of GatewaySendEvent
     | Receive of GatewayReceiveEvent * string
+
+    | SetActive of bool
     | Queue of GatewaySendQueue.Msg
     | Ignore
 
 let init (identifyEvent, handler) =
     let sendQueue, sendQueueCmd = GatewaySendQueue.init()
+
     {
         IdentifyEvent = identifyEvent
         Handler = handler
-        SequenceId = None
+        Active = false
         Interval = None
-        Heartbeat = None
-        HeartbeatAcked = true
         ResumeGatewayUrl = None
         SessionId = None
+        SequenceId = None
+        Heartbeat = None
+        HeartbeatAcked = true
         SendQueue = sendQueue
     },
-    Cmd.batch [
-        Cmd.map Msg.Queue sendQueueCmd
-    ]
+    Cmd.map Msg.Queue sendQueueCmd
 
-let private send model (ev: GatewaySendEvent) =
-    model, Cmd.ofMsg (Msg.Queue (GatewaySendQueue.Msg.Enqueue ev))
-
-let private receive model (ev: GatewayReceiveEvent) (raw: string) =
-    match ev with
-    | GatewayReceiveEvent.HELLO ev ->
+let private receive (env: #IGetCurrentTime) model (ev: GatewayReceiveEvent) (raw: string) =
+    match ev, raw with
+    | GatewayReceiveEvent.HELLO ev, _ ->
         match model.SessionId, model.SequenceId with
         | Some sessionId, Some sequenceId ->
             let event = GatewayEventPayload.create(GatewayOpcode.RESUME, ResumeSendEvent.create(model.IdentifyEvent.Token, sessionId, sequenceId)) |> GatewaySendEvent.RESUME
@@ -63,38 +63,54 @@ let private receive model (ev: GatewayReceiveEvent) (raw: string) =
             { model with Interval = Some ev.Data.HeartbeatInterval },
             Cmd.ofMsg (Msg.Send event)
 
-    | GatewayReceiveEvent.HEARTBEAT ev ->
+    | GatewayReceiveEvent.HEARTBEAT ev, _ ->
         let event = GatewayEventPayload.create(GatewayOpcode.HEARTBEAT, model.SequenceId) |> GatewaySendEvent.HEARTBEAT
 
-        { model with Heartbeat = model.Interval |> Option.map DateTime.UtcNow.AddMilliseconds }, // TODO: Remove current time dependency
+        { model with Heartbeat = model.Interval |> Option.map (env.GetCurrentTime().AddMilliseconds) },
         Cmd.ofMsg (Msg.Send event)
 
-    | GatewayReceiveEvent.HEARTBEAT_ACK ev ->
+    | GatewayReceiveEvent.HEARTBEAT_ACK ev, _ ->
         { model with HeartbeatAcked = true }, Cmd.none
 
-    | GatewayReceiveEvent.READY ev ->
-        { model with ResumeGatewayUrl = Some ev.Data.ResumeGatewayUrl; SessionId = Some ev.Data.SessionId }, Cmd.none
+    | GatewayReceiveEvent.READY ev, _ ->
+        { model with ResumeGatewayUrl = Some ev.Data.ResumeGatewayUrl; SessionId = Some ev.Data.SessionId },
+        Cmd.ofMsg (Msg.SetActive true)
 
-    | GatewayReceiveEvent.RESUMED ev ->
-        model, Cmd.none // TODO: Mark state as active? Same with ready?
+    | GatewayReceiveEvent.RESUMED _, _ ->
+        model, Cmd.ofMsg (Msg.SetActive true)
 
-    | GatewayReceiveEvent.RECONNECT ev ->
+    | GatewayReceiveEvent.RECONNECT _, _ ->
         match model.ResumeGatewayUrl, model.SessionId, model.SequenceId with
-        | Some resumeGatewayUrl, Some sessionId, Some sequenceId -> model, Cmd.none // TODO: Resume
-        | _ -> model, Cmd.none // TODO: Reconnect
+        | Some _, Some _, Some _ -> model, Cmd.ofMsg (Msg.SetActive false) // TODO: Resume
+        | _ -> model, Cmd.ofMsg (Msg.SetActive false) // TODO: Reconnect
 
-    | GatewayReceiveEvent.INVALID_SESSION ev ->
+    | GatewayReceiveEvent.INVALID_SESSION ev, _ ->
         match ev.Data, model.ResumeGatewayUrl, model.SessionId, model.SequenceId with
-        | true, Some resumeGatewayUrl, Some sessionId, Some sequenceId -> model, Cmd.none // TODO: Resume
-        | _ -> model, Cmd.none // TODO: Reconnect
+        | true, Some _, Some _, Some _ -> model, Cmd.ofMsg (Msg.SetActive false) // TODO: Resume
+        | _ -> model, Cmd.ofMsg (Msg.SetActive false) // TODO: Reconnect
 
-    | ev ->
+    | _, raw ->
         model, Cmd.OfAsync.perform (fun r -> model.Handler r |> Async.AwaitTask) raw (fun _ -> Msg.Ignore)
+
+let private setActive model active =
+    let opcode =
+        match active with
+        | false -> CustomGatewayOpcode.INACTIVE
+        | true -> CustomGatewayOpcode.ACTIVE
+
+    let ev =
+        GatewayReceiveEvent.UNKNOWN (GatewayEventPayload.create (CustomGatewayOpcode.cast opcode, ())),
+        $"""{{"op": {CustomGatewayOpcode.toString opcode}}}"""
+    
+    // TODO: Clean up this (should this even be sending in the same way as a custom event?)
+    
+    { model with Active = active }, Cmd.ofMsg (Msg.Receive ev)
 
 let update env msg model =
     match msg with
-    | Msg.Send ev -> send model ev
-    | Msg.Receive (ev, raw) -> receive model ev raw
+    | Msg.Send ev -> model, Cmd.ofMsg (Msg.Queue (GatewaySendQueue.Msg.Enqueue ev))
+    | Msg.Receive (ev, raw) -> receive env model ev raw
+    | Msg.SetActive active -> setActive model active
     | Msg.Queue msg' ->
         let res, cmd = GatewaySendQueue.update env msg' model.SendQueue
         { model with SendQueue = res }, Cmd.map Msg.Queue cmd
@@ -105,5 +121,8 @@ let view model dispatch =
 
 let program env identify handler =
     Program.mkProgram init (update env) view
-    // TODO: Add subscriptions to trigger send and receive messages with the websocket here `|> Program.withSubscription ...` (?)
+    // TODO: Add subscription to shutdown connection `|> Program.withSubscription ...` (or maybe its `|> Program.withTermination ...` ?)
     |> Program.runWith (identify, handler)
+
+// TODO: Setup and manage websocket internally
+// TODO: Figure out how to notify that the gateway disconencted irrecoverably (likely cant use `Program.runWith` as it starts single threaded loop)
