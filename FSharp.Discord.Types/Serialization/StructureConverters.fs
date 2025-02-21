1b1213a0c1cb725ebe856a2eea1ba31cd6fa8338
leydel <me@itsf.in>
2025-02-21 15:31:56 +1000

    Update encoder and decoder function signatures

--- FSharp.Discord.Types/Serialization/StructureConverters.fs
+++ FSharp.Discord.Types/Serialization/StructureConverters.fs
@@ -18,59 +18,63 @@ module Encode =
         |> Encode.dict
 
 module ErrorResponse =
-    let decoder: Decoder<_> = Decode.object (fun get -> {
-        Code = get.Required.Field "code" Decode.Enum.int<JsonErrorCode>
-        Message = get.Required.Field "message" Decode.string
-        Errors = get.Required.Field "errors" (Decode.dict Decode.string)
-    })
+    let decoder path v =
+        Decode.object (fun get -> {
+            Code = get.Required.Field "code" Decode.Enum.int<JsonErrorCode>
+            Message = get.Required.Field "message" Decode.string
+            Errors = get.Required.Field "errors" (Decode.dict Decode.string)
+        }) path v
 
-    let encoder (errorResponse: ErrorResponse) = Encode.object [
-        "code", Encode.Enum.int errorResponse.Code
-        "message", Encode.string errorResponse.Message
-        "errors", Encode.mapv Encode.string errorResponse.Errors
-    ]
+    let encoder (v: ErrorResponse) =
+        Encode.object [
+            "code", Encode.Enum.int v.Code
+            "message", Encode.string v.Message
+            "errors", Encode.mapv Encode.string v.Errors
+        ]
 
 //module Interaction =
-//    let decoder = Decode.object (fun get -> {
-//        Id = get.Required.Field "id" Decode.string
-//        ApplicationId = get.Required.Field "application_id" Decode.string
-//        Type = get.Required.Field "type" Decode.Enum.int<InteractionType>
-//        Data = get.Optional.Field "data" InteractionData.decoder
-//        Guild = get.Optional.Field "guild" PartialGuild.decoder
-//        GuildId = get.Optional.Field "guild_id" Decode.string
-//        Channel = get.Optional.Field "channel" PartialChannel.decoder
-//        ChannelId = get.Optional.Field "channel_id" Decode.string
-//        Member = get.Optional.Field "member" GuildMember.decoder
-//        User = get.Optional.Field "user" User.decoder
-//        Token = get.Required.Field "token" Decode.string
-//        Version = get.Required.Field "version" Decode.int
-//        Message = get.Optional.Field "message" Message.decoder
-//        AppPermissions = get.Required.Field "app_permissions" Decode.string
-//        Locale = get.Optional.Field "locale" Decode.string
-//        GuildLocale = get.Optional.Field "guild_locale" Decode.string
-//        Entitlements = get.Required.Field "entitlements" (Decode.list Entitlement.decoder)
-//        AuthorizingIntegrationOwners = get.Required.Field "authorizing_integration_owners" (Decode.dict ApplicationIntegrationTypeConfiguration.decoder) // TODO: Ensure key is a valid option
-//        Context = get.Optional.Field "context" Decode.Enum.int<InteractionContextType>
-//    })
+//    let decoder path v =
+//        Decode.object (fun get -> {
+//            Id = get.Required.Field "id" Decode.string
+//            ApplicationId = get.Required.Field "application_id" Decode.string
+//            Type = get.Required.Field "type" Decode.Enum.int<InteractionType>
+//            Data = get.Optional.Field "data" InteractionData.decoder
+//            Guild = get.Optional.Field "guild" PartialGuild.decoder
+//            GuildId = get.Optional.Field "guild_id" Decode.string
+//            Channel = get.Optional.Field "channel" PartialChannel.decoder
+//            ChannelId = get.Optional.Field "channel_id" Decode.string
+//            Member = get.Optional.Field "member" GuildMember.decoder
+//            User = get.Optional.Field "user" User.decoder
+//            Token = get.Required.Field "token" Decode.string
+//            Version = get.Required.Field "version" Decode.int
+//            Message = get.Optional.Field "message" Message.decoder
+//            AppPermissions = get.Required.Field "app_permissions" Decode.string
+//            Locale = get.Optional.Field "locale" Decode.string
+//            GuildLocale = get.Optional.Field "guild_locale" Decode.string
+//            Entitlements = get.Required.Field "entitlements" (Decode.list Entitlement.decoder)
+//            AuthorizingIntegrationOwners = get.Required.Field "authorizing_integration_owners" (Decode.dict ApplicationIntegrationTypeConfiguration.decoder) // TODO: Ensure key is a valid option
+//            Context = get.Optional.Field "context" Decode.Enum.int<InteractionContextType>
+//        }) path v
 
-//    let encoder (interaction: Interaction) = Encode.object [
-//        "id", Encode.string interaction.Id
-//        "application_id", Encode.string interaction.ApplicationId
-//        "type", Encode.Enum.int interaction.Type
-//        "data", Encode.option InteractionData.encoder interaction.Data
-//        "guild", Encode.option Guild.encoder interaction.Guild
-//        "guild_id", Encode.option Encode.string interaction.GuildId
-//        "channel", Encode.option PartialChannel.encoder interaction.Channel
-//        "channel_id", Encode.option Encode.string interaction.ChannelId
-//        "member", Encode.option GuildMember.encoder interaction.Member
-//        "user", Encode.option User.encoder interaction.User
-//        "token", Encode.string interaction.Token
-//        "version", Encode.int interaction.Version
-//        "message", Encode.option Message.encoder interaction.Message
-//        "app_permissions", Encode.string interaction.AppPermissions
-//        "locale", Encode.option Encode.string interaction.Locale
-//        "guild_locale", Encode.option Encode.string interaction.GuildLocale
-//        "entitlements", Encode.list <| List.map Entitlement.encoder interaction.Entitlements
-//        "authorizing_integration_owners", Encode.mapv ApplicationIntegrationTypeConfiguration.encoder interaction.AuthorizingIntegrationOwners
-//        "context", Encode.option Encode.Enum.int interaction.Context
-//    ]
+//    let encoder (v: Interaction) =
+//        Encode.object [
+//            "id", Encode.string v.Id
+//            "application_id", Encode.string v.ApplicationId
+//            "type", Encode.Enum.int v.Type
+//            "data", Encode.option InteractionData.encoder v.Data
+//            "guild", Encode.option Guild.encoder v.Guild
+//            "guild_id", Encode.option Encode.string v.GuildId
+//            "channel", Encode.option PartialChannel.encoder v.Channel
+//            "channel_id", Encode.option Encode.string v.ChannelId
+//            "member", Encode.option GuildMember.encoder v.Member
+//            "user", Encode.option User.encoder v.User
+//            "token", Encode.string v.Token
+//            "version", Encode.int v.Version
+//            "message", Encode.option Message.encoder v.Message
+//            "app_permissions", Encode.string v.AppPermissions
+//            "locale", Encode.option Encode.string v.Locale
+//            "guild_locale", Encode.option Encode.string v.GuildLocale
+//            "entitlements", Encode.list <| List.map Entitlement.encoder v.Entitlements
+//            "authorizing_integration_owners", Encode.mapv ApplicationIntegrationTypeConfiguration.encoder v.AuthorizingIntegrationOwners
+//            "context", Encode.option Encode.Enum.int v.Context
+//        ]
