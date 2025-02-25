c8824b5502ee68146606fb4fcf41ed511944af3f
leydel <me@itsf.in>
2025-02-25 12:34:43 +1000

    Add check to ensure event body data is defined before accessing option value

--- FSharp.Discord.Webhook/WebhookReceiveEventConverter.fs
+++ FSharp.Discord.Webhook/WebhookReceiveEventConverter.fs
@@ -28,12 +28,18 @@ module WebhookReceiveEvent =
                 |> fun f -> f path v
                 |> Result.bind (function | Some v -> Ok v | None -> Error (path, BadPrimitive("a webhook event type", v)))
 
-            let event (v: WebhookEventPayload<'a>) = {
-                Version = v.Version
-                ApplicationId = v.ApplicationId
-                Timestamp = v.Event.Value.Timestamp
-                Data = v.Event.Value.Data.Value
-            }
+            let event (payload: WebhookEventPayload<'a>) =
+                match payload.Event.Value.Data with
+                | None -> Error (path, BadType("missing webhook event data", v))
+                | Some data ->
+                    Ok {
+                        Version = payload.Version
+                        ApplicationId = payload.ApplicationId
+                        Timestamp = payload.Event.Value.Timestamp
+                        Data = data
+                    }
+
+                // TODO: Clean this up so it doesnt use `.Value` (it is true based on how `eventType` works but not obvious)
 
             match eventType with
             | Error err ->
@@ -41,11 +47,13 @@ module WebhookReceiveEvent =
 
             | Ok WebhookEventType.ENTITLEMENT_CREATE ->
                 WebhookEventPayload.decoder EntitlementCreateEvent.decoder path v
-                |> Result.map (fun v -> WebhookReceiveEvent.ENTITLEMENT_CREATE (event v))
+                |> Result.bind event
+                |> Result.map WebhookReceiveEvent.ENTITLEMENT_CREATE
 
             | Ok WebhookEventType.APPLICATION_AUTHORIZED ->
                 WebhookEventPayload.decoder ApplicationAuthorizedEvent.decoder path v
-                |> Result.map (fun v -> WebhookReceiveEvent.APPLICATION_AUTHORIZED (event v))
+                |> Result.bind event
+                |> Result.map WebhookReceiveEvent.APPLICATION_AUTHORIZED
 
         | _ ->
             Error (path, BadPrimitive("a webhook payload type", v))
