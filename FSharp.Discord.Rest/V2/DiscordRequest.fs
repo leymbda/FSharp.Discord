4141ea8ef82ffb8974b46bb08f7cf948769221b9
FinOCE <me@itsf.in>
2025-03-15 14:47:59 +1000

    Start V2 rest modules to handling thoth serializer refactoring

---
+++ FSharp.Discord.Rest/V2/DiscordRequest.fs
@@ -0,0 +1,12 @@
+﻿module FSharp.Discord.Rest.V2.DiscordRequest
+
+open System.Net.Http
+open System.Web
+
+/// Append the audit log reason header to the request.
+let withAuditLogReason (auditLogReason: string option) (req: HttpRequestMessage) =
+    let folder (req: HttpRequestMessage) (auditLogReason: string) =
+        req.Headers.Add("X-Audit-Log-Reason", HttpUtility.UrlEncode auditLogReason)
+        req
+
+    Option.fold folder req auditLogReason
---
+++ FSharp.Discord.Rest/V2/RequestBuilder.fs
@@ -0,0 +1,3 @@
+﻿namespace FSharp.Discord.Rest.V2
+
+// TODO: Implement an appropriate form of request builder (should it still even be a computation expression?)
