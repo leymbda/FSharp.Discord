2d2610b58dabdd124f36627b15e15f45712a50a9
FinOCE <me@itsf.in>
2025-03-25 00:20:16 +1000

    Separate payloads from contracts

---
+++ FSharp.Discord.Rest/Payloads.fs
@@ -0,0 +1,340 @@
+﻿namespace FSharp.Discord.Rest
+
+open FSharp.Discord.Types
+open FSharp.Discord.Types.Serialization
+open Thoth.Json.Net
+
+// ----- Interactions: Receiving and Responding -----
+
+type CreateInteractionResponsePayload(response, ?files) =
+    member val Response: InteractionResponse = response
+    member val Files: Media list = defaultArg files []
+
+    interface IPayload with
+        member this.ToHttpContent() =
+            HttpContent.createJsonWithFiles InteractionResponse.encoder this.Response this.Files
+            
+type EditOriginalInteractionResponsePayload(
+    ?content, ?embeds, ?allowedMentions, ?components, ?attachments, ?poll, ?files
+) =
+    member val Content: string option option = content
+    member val Embeds: Embed list option option = embeds
+    member val AllowedMentions: AllowedMentions option option = allowedMentions
+    member val Components: Component list option option = components
+    member val Attachments: Attachment list option option = attachments
+    member val Poll: Poll option option = poll
+    member val Files: Media list = defaultArg files []
+
+    static member Encoder(v: EditOriginalInteractionResponsePayload) =
+        Encode.object ([]
+            |> Encode.optinull "content" Encode.string v.Content
+            |> Encode.optinull "embeds" (List.map Embed.encoder >> Encode.list) v.Embeds
+            |> Encode.optinull "allowed_mentions" AllowedMentions.encoder v.AllowedMentions
+            |> Encode.optinull "components" (List.map Component.encoder >> Encode.list) v.Components
+            |> Encode.optinull "attachments" (List.map Attachment.encoder >> Encode.list) v.Attachments
+            |> Encode.optinull "poll" Poll.encoder v.Poll
+        )
+    
+    interface IPayload with
+        member this.ToHttpContent() =
+            HttpContent.createJsonWithFiles EditOriginalInteractionResponsePayload.Encoder this this.Files
+           
+type CreateFollowUpMessagePayload(
+    ?content, ?tts, ?embeds, ?allowedMentions, ?components, ?attachments, ?flags, ?poll, ?files
+) =
+    member val Content: string option = content
+    member val Tts: bool option = tts
+    member val Embeds: Embed list option = embeds
+    member val AllowedMentions: AllowedMentions option = allowedMentions
+    member val Components: Component list option = components
+    member val Attachments: Attachment list option = attachments
+    member val Flags: int option = flags
+    member val Poll: Poll option = poll
+    member val Files: Media list = defaultArg files []
+
+    static member Encoder(v: CreateFollowUpMessagePayload) =
+        Encode.object ([]
+            |> Encode.optional "content" Encode.string v.Content
+            |> Encode.optional "tts" Encode.bool v.Tts
+            |> Encode.optional "embeds" (List.map Embed.encoder >> Encode.list) v.Embeds
+            |> Encode.optional "allowed_mentions" AllowedMentions.encoder v.AllowedMentions
+            |> Encode.optional "components" (List.map Component.encoder >> Encode.list) v.Components
+            |> Encode.optional "attachments" (List.map Attachment.encoder >> Encode.list) v.Attachments
+            |> Encode.optional "flags" Encode.int v.Flags
+            |> Encode.optional "poll" Poll.encoder v.Poll
+        )
+    
+    interface IPayload with
+        member this.ToHttpContent() =
+            HttpContent.createJsonWithFiles CreateFollowUpMessagePayload.Encoder this this.Files
+
+    // TODO: Change flags to list of message flags
+    // TODO: Should this support thread_name and applied_tags? Not explicitly refused in docs but wouldn't make sense
+    
+type EditFollowupMessagePayload(?content, ?embeds, ?allowedMentions, ?components, ?attachments, ?poll, ?files) =
+    member val Content: string option option = content
+    member val Embeds: Embed list option option = embeds
+    member val AllowedMentions: AllowedMentions option option = allowedMentions
+    member val Components: Component list option option = components
+    member val Attachments: Attachment list option option = attachments
+    member val Poll: Poll option option = poll
+    member val Files: Media list = defaultArg files []
+
+    static member Encoder(v: EditFollowupMessagePayload) =
+        Encode.object ([]
+            |> Encode.optinull "content" Encode.string v.Content
+            |> Encode.optinull "embeds" (List.map Embed.encoder >> Encode.list) v.Embeds
+            |> Encode.optinull "allowed_mentions" AllowedMentions.encoder v.AllowedMentions
+            |> Encode.optinull "components" (List.map Component.encoder >> Encode.list) v.Components
+            |> Encode.optinull "attachments" (List.map Attachment.encoder >> Encode.list) v.Attachments
+            |> Encode.optinull "poll" Poll.encoder v.Poll
+        )
+    
+    interface IPayload with
+        member this.ToHttpContent() =
+            HttpContent.createJsonWithFiles EditFollowupMessagePayload.Encoder this this.Files
+            
+// ----- Interactions: Application Commands -----
+
+type CreateGlobalApplicationCommandPayload(
+    name, ?nameLocalizations, ?description, ?descriptionLocalizations, ?options, ?defaultMemberPermissions,
+    ?integrationTypes, ?contexts, ?type', ?nsfw
+) =
+    member val Name: string = name
+    member val NameLocalizations: Map<string, string> option option = nameLocalizations
+    member val Description: string option = description
+    member val DescriptionLocalizations: Map<string, string> option option = descriptionLocalizations
+    member val Options: ApplicationCommandOption list option = options
+    member val DefaultMemberPermissions: string option option = defaultMemberPermissions
+    member val IntegrationTypes: ApplicationIntegrationType list option = integrationTypes
+    member val Contexts: InteractionContextType list option = contexts
+    member val Type: ApplicationCommandType option = type'
+    member val Nsfw: bool option = nsfw
+    
+    static member Encoder(v: CreateGlobalApplicationCommandPayload) =
+        Encode.object ([]
+            |> Encode.required "name" Encode.string v.Name
+            |> Encode.optinull "name_localizations" (Encode.mapv Encode.string) v.NameLocalizations
+            |> Encode.optional "description" Encode.string v.Description
+            |> Encode.optinull "description_localizations" (Encode.mapv Encode.string) v.DescriptionLocalizations
+            |> Encode.optional "options" (List.map ApplicationCommandOption.encoder >> Encode.list) v.Options
+            |> Encode.optinull "default_member_permissions" Encode.string v.DefaultMemberPermissions
+            |> Encode.optional "integration_types" (List.map Encode.Enum.int<ApplicationIntegrationType> >> Encode.list) v.IntegrationTypes
+            |> Encode.optional "contexts" (List.map Encode.Enum.int<InteractionContextType> >> Encode.list) v.Contexts
+            |> Encode.optional "type" Encode.Enum.int<ApplicationCommandType> v.Type
+            |> Encode.optional "nsfw" Encode.bool v.Nsfw
+        )
+    
+    interface IPayload with
+        member this.ToHttpContent() =
+            StringContent.createJson CreateGlobalApplicationCommandPayload.Encoder this
+            
+type EditGlobalApplicationCommandPayload(
+    ?name, ?nameLocalizations, ?description, ?descriptionLocalizations, ?options, ?defaultMemberPermissions,
+    ?integrationTypes, ?contexts, ?type', ?nsfw
+) =
+    member val Name: string option = name
+    member val NameLocalizations: Map<string, string> option option = nameLocalizations
+    member val Description: string option = description
+    member val DescriptionLocalizations: Map<string, string> option option = descriptionLocalizations
+    member val Options: ApplicationCommandOption list option = options
+    member val DefaultMemberPermissions: bool option option = defaultMemberPermissions
+    member val IntegrationTypes: ApplicationIntegrationType list option = integrationTypes
+    member val Contexts: InteractionContextType list option = contexts
+    member val Nsfw: bool option = nsfw
+    
+    static member Encoder(v: EditGlobalApplicationCommandPayload) =
+        Encode.object ([]
+            |> Encode.optional "name" Encode.string v.Name
+            |> Encode.optinull "name_localizations" (Encode.mapv Encode.string) v.NameLocalizations
+            |> Encode.optional "description" Encode.string v.Description
+            |> Encode.optinull "description_localizations" (Encode.mapv Encode.string) v.DescriptionLocalizations
+            |> Encode.optional "options" (List.map ApplicationCommandOption.encoder >> Encode.list) v.Options
+            |> Encode.optinull "default_member_permissions" Encode.bool v.DefaultMemberPermissions
+            |> Encode.optional "integration_types" (List.map Encode.Enum.int<ApplicationIntegrationType> >> Encode.list) v.IntegrationTypes
+            |> Encode.optional "contexts" (List.map Encode.Enum.int<InteractionContextType> >> Encode.list) v.Contexts
+            |> Encode.optional "nsfw" Encode.bool v.Nsfw
+        )
+    
+    interface IPayload with
+        member this.ToHttpContent() =
+            StringContent.createJson EditGlobalApplicationCommandPayload.Encoder this
+            
+type BulkOverwriteGlobalApplicationCommandsPayload(commands) =
+    member val Commands: CreateGlobalApplicationCommandPayload list = commands
+    
+    static member Encoder(v: BulkOverwriteGlobalApplicationCommandsPayload) =
+        (List.map CreateGlobalApplicationCommandPayload.Encoder >> Encode.list) v.Commands
+    
+    interface IPayload with
+        member this.ToHttpContent() =
+            StringContent.createJson BulkOverwriteGlobalApplicationCommandsPayload.Encoder this
+
+    // TODO: Docs appear incorrect currently, likely also contains `id` like bulk guild commands, which also looks partially incorrectly documented
+    
+type CreateGuildApplicationCommandPayload(
+    name, ?nameLocalizations, ?description, ?descriptionLocalizations, ?options, ?defaultMemberPermissions,
+    ?type', ?nsfw
+) =
+    member val Name: string = name
+    member val NameLocalizations: Map<string, string> option option = nameLocalizations
+    member val Description: string option = description
+    member val DescriptionLocalizations: Map<string, string> option option = descriptionLocalizations
+    member val Options: ApplicationCommandOption list option = options
+    member val DefaultMemberPermissions: string option option = defaultMemberPermissions
+    member val Type: ApplicationCommandType option = type'
+    member val Nsfw: bool option = nsfw
+    
+    static member Encoder(v: CreateGuildApplicationCommandPayload) =
+        Encode.object ([]
+            |> Encode.required "name" Encode.string v.Name
+            |> Encode.optinull "name_localizations" (Encode.mapv Encode.string) v.NameLocalizations
+            |> Encode.optional "description" Encode.string v.Description
+            |> Encode.optinull "description_localizations" (Encode.mapv Encode.string) v.DescriptionLocalizations
+            |> Encode.optional "options" (List.map ApplicationCommandOption.encoder >> Encode.list) v.Options
+            |> Encode.optinull "default_member_permissions" Encode.string v.DefaultMemberPermissions
+            |> Encode.optional "type" Encode.Enum.int<ApplicationCommandType> v.Type
+            |> Encode.optional "nsfw" Encode.bool v.Nsfw
+        )
+    
+    interface IPayload with
+        member this.ToHttpContent() =
+            StringContent.createJson CreateGuildApplicationCommandPayload.Encoder this
+            
+type EditGuildApplicationCommandPayload(
+    ?name, ?nameLocalizations, ?description, ?descriptionLocalizations, ?options, ?defaultMemberPermissions, ?nsfw
+) =
+    member val Name: string option = name
+    member val NameLocalizations: Map<string, string> option option = nameLocalizations
+    member val Description: string option = description
+    member val DescriptionLocalizations: Map<string, string> option option = descriptionLocalizations
+    member val Options: ApplicationCommandOption list option = options
+    member val DefaultMemberPermissions: bool option option = defaultMemberPermissions
+    member val Nsfw: bool option = nsfw
+    
+    static member Encoder(v: EditGuildApplicationCommandPayload) =
+        Encode.object ([]
+            |> Encode.optional "name" Encode.string v.Name
+            |> Encode.optinull "name_localizations" (Encode.mapv Encode.string) v.NameLocalizations
+            |> Encode.optional "description" Encode.string v.Description
+            |> Encode.optinull "description_localizations" (Encode.mapv Encode.string) v.DescriptionLocalizations
+            |> Encode.optional "options" (List.map ApplicationCommandOption.encoder >> Encode.list) v.Options
+            |> Encode.optinull "default_member_permissions" Encode.bool v.DefaultMemberPermissions
+            |> Encode.optional "nsfw" Encode.bool v.Nsfw
+        )
+    
+    interface IPayload with
+        member this.ToHttpContent() =
+            StringContent.createJson EditGuildApplicationCommandPayload.Encoder this
+            
+type BulkOverwriteGuildApplicationCommandsPayload(commands) =
+    member val Commands: CreateGuildApplicationCommandPayload list = commands
+    
+    static member Encoder(v: BulkOverwriteGuildApplicationCommandsPayload) =
+        (List.map CreateGuildApplicationCommandPayload.Encoder >> Encode.list) v.Commands
+    
+    interface IPayload with
+        member this.ToHttpContent() =
+            StringContent.createJson BulkOverwriteGuildApplicationCommandsPayload.Encoder this
+
+    // TODO: Also contains optional `id` property "ID of the command, if known"
+    // TODO: Docs include `integration_types` and `contexts` but these should be global only afaik
+    
+type EditApplicationCommandPermissionsPayload(permissions) =
+    member val Permissions: ApplicationCommandPermission list = permissions
+    
+    static member Encoder(v: EditApplicationCommandPermissionsPayload) =
+        Encode.object ([]
+            |> Encode.required "permissions" (List.map ApplicationCommandPermission.encoder >> Encode.list) v.Permissions
+        )
+    
+    interface IPayload with
+        member this.ToHttpContent() =
+            StringContent.createJson EditApplicationCommandPermissionsPayload.Encoder this
+            
+// ----- Events: Using Gateway -----
+
+// ----- Resources: Application -----
+
+type EditCurrentApplicationPayload(
+    customInstallUrl, description, roleConnectionsVerificationUrl, installParams, integrationTypesConfig, flags, icon,
+    coverImage, interactionsEndpointUrl, tags, eventWebhooksUrl, eventWebhooksStatus, eventWebhooksTypes
+) =
+    member val CustomInstallUrl: string option = customInstallUrl
+    member val Description: string option = description
+    member val RoleConnectionsVerificationUrl: string option = roleConnectionsVerificationUrl
+    member val InstallParams: InstallParams option = installParams
+    member val IntegrationTypesConfig: Map<ApplicationIntegrationType, ApplicationIntegrationTypeConfiguration> option = integrationTypesConfig
+    member val Flags: int option = flags
+    member val Icon: string option option = icon
+    member val CoverImage: string option option = coverImage
+    member val InteractionsEndpointUrl: string option = interactionsEndpointUrl
+    member val Tags: string list option = tags
+    member val EventWebhooksUrl: string option = eventWebhooksUrl
+    member val EventWebhooksStatus: WebhookEventStatus option = eventWebhooksStatus
+    member val EventWebhooksTypes: WebhookEventType list option = eventWebhooksTypes
+
+    static member Encoder (v: EditCurrentApplicationPayload) =
+        Encode.object ([]
+            |> Encode.optional "custom_install_url" Encode.string v.CustomInstallUrl
+            |> Encode.optional "description" Encode.string v.Description
+            |> Encode.optional "role_connections_verification_url" Encode.string v.RoleConnectionsVerificationUrl
+            |> Encode.optional "install_params" InstallParams.encoder v.InstallParams
+            |> Encode.optional "integration_types_config" (Encode.mapkv ApplicationIntegrationType.toString ApplicationIntegrationTypeConfiguration.encoder) v.IntegrationTypesConfig
+            |> Encode.optional "flags" Encode.int v.Flags
+            |> Encode.optinull "icon" Encode.string v.Icon
+            |> Encode.optinull "cover_image" Encode.string v.CoverImage
+            |> Encode.optional "interactions_endpoint_url" Encode.string v.InteractionsEndpointUrl
+            |> Encode.optional "tags" (List.map Encode.string >> Encode.list) v.Tags
+            |> Encode.optional "event_webhooks_url" Encode.string v.EventWebhooksUrl
+            |> Encode.optional "event_webhooks_status" Encode.Enum.int v.EventWebhooksStatus
+            |> Encode.optional "event_webhooks_types" (List.map WebhookEventType.encoder >> Encode.list) v.EventWebhooksTypes
+        )
+        
+    interface IPayload with
+        member this.ToHttpContent() =
+            StringContent.createJson EditCurrentApplicationPayload.Encoder this
+            
+// ----- Resources: Application Role Connection Metadata -----
+
+// ----- Resources: Audit Log -----
+
+// ----- Resources: Auto Moderation -----
+
+// ----- Resources: Channel -----
+
+// ----- Resources: Emoji -----
+
+// ----- Resources: Entitlement -----
+
+// ----- Resources: Guild -----
+
+// ----- Resources: Guild Scheduled Event -----
+
+// ----- Resources: Guild Template -----
+
+// ----- Resources: Invite -----
+
+// ----- Resources: Lobby -----
+
+// ----- Resources: Message -----
+
+// ----- Resources: Poll -----
+
+// ----- Resources: SKU -----
+
+// ----- Resources: Soundboard -----
+
+// ----- Resources: Stage Instance -----
+
+// ----- Resources: Sticker -----
+
+// ----- Resources: Subscription -----
+
+// ----- Resources: User -----
+
+// ----- Resources: Voice -----
+
+// ----- Resources: Webhook -----
+
+// ----- Topics: OAuth2 -----
