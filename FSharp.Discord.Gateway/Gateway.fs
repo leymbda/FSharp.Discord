b9a77091201720d04b76b66cf5d7423e9d6b6ede
leydel <me@itsf.in>
2025-04-11 16:07:49 +1000

    Create gateway connection to manage socket connection and gateway for abstracted management

--- FSharp.Discord.Gateway/Gateway.fs
+++ FSharp.Discord.Gateway/Gateway.fs
@@ -1,59 +1,46 @@
 ﻿namespace FSharp.Discord.Gateway
 
-open FSharp.Discord.Types
-open FSharp.Discord.Types.Serialization
-open Thoth.Json.Net
-open WebSocketSharp
+open System
 
-type GatewayState = {
-    Sequence: int option
-    // TODO: Add other relevant lifecycle state here
-}
-
-type Gateway = {
-    Socket: WebSocket
-    State: GatewayState option
-}
+type Gateway(gatewayUrl) =
+    member val GatewayUrl: string = gatewayUrl
+    member val Connection: GatewayConnection option = None with get, set
+    
+    interface IDisposable with
+        member this.Dispose() =
+            this.Connection |> Option.iter (fun c -> c :> IDisposable |> _.Dispose())
 
 module Gateway =
     let create gatewayUrl =
-        let ws = new WebSocket(gatewayUrl)
+        new Gateway(gatewayUrl)
 
-        ws.OnOpen.Add(fun _ -> ()) // TODO: Handle initiating lifecycle
-        ws.OnMessage.Add(fun (e: MessageEventArgs) -> ()) // TODO: Handle lifecycle events then event handler
-        ws.OnError.Add(fun (e: ErrorEventArgs) -> ()) // TODO: Figure out when this occurs and what it should handle
-        ws.OnClose.Add(fun (e: CloseEventArgs) -> ()) // TODO: Handle resume/reconnect if appropriate (involves creating new ws ??)
+    let connect (gateway: Gateway) =
+        match gateway.Connection with
+        | Some _ ->
+            Error "Connection already exists"
 
-        ws
-
-    let private send event gateway =
-        gateway.State |> Option.iter (fun state ->
-            match event with
-            | GatewaySendEvent.REQUEST_GUILD_MEMBERS d ->
-                Some {
-                    Opcode = GatewayOpcode.REQUEST_GUILD_MEMBERS
-                    Data = GatewaySendEventData.REQUEST_GUILD_MEMBERS d
-                    Sequence = state.Sequence
-                    EventName = None
-                }
-
-            | _ ->
-                None
-
-            |> Option.iter (GatewaySendEventPayload.encoder >> Encode.toString 0 >> gateway.Socket.Send)
-        )
-
-    let requestGuildMembers event gateway =
-        send (GatewaySendEvent.REQUEST_GUILD_MEMBERS event) gateway
-
-    let requestSoundboardSounds event gateway =
-        send (GatewaySendEvent.REQUEST_SOUNDBOARD_SOUNDS event) gateway
-
-    let updateVoiceState event gateway =
-        send (GatewaySendEvent.UPDATE_VOICE_STATE event) gateway
-
-    let updatePresence event gateway =
-        send (GatewaySendEvent.UPDATE_PRESENCE event) gateway
+        | None ->
+            gateway.Connection <- Some (GatewayConnection.create gateway.GatewayUrl None)
+            Ok ()
 
     let close (gateway: Gateway) =
-        gateway.Socket.Close()
+        match gateway.Connection with
+        | None ->
+            Error "No connection exists"
+
+        | Some connection ->
+            GatewayConnection.close connection
+            gateway.Connection <- None
+            Ok ()
+        
+    let requestGuildMembers event (gateway: Gateway) =
+        gateway.Connection |> Option.iter (GatewayConnection.send (GatewaySendEvent.REQUEST_GUILD_MEMBERS event))
+
+    let requestSoundboardSounds event (gateway: Gateway) =
+        gateway.Connection |> Option.iter (GatewayConnection.send (GatewaySendEvent.REQUEST_SOUNDBOARD_SOUNDS event))
+
+    let updateVoiceState event (gateway: Gateway) =
+        gateway.Connection |> Option.iter (GatewayConnection.send (GatewaySendEvent.UPDATE_VOICE_STATE event))
+
+    let updatePresence event (gateway: Gateway) =
+        gateway.Connection |> Option.iter (GatewayConnection.send (GatewaySendEvent.UPDATE_PRESENCE event))
---
+++ FSharp.Discord.Gateway/GatewayConnection.fs
@@ -0,0 +1,80 @@
+﻿namespace FSharp.Discord.Gateway
+
+open FSharp.Discord.Types
+open FSharp.Discord.Types.Serialization
+open System
+open Thoth.Json.Net
+open WebSocketSharp
+
+type GatewayConnectionState = {
+    Sequence: int option
+    // TODO: Add other relevant lifecycle state here
+}
+
+module GatewayConnectionState =
+    let create () = {
+        Sequence = None
+    }
+
+type GatewayConnection(socket, state) =
+    member val Socket: WebSocket = socket
+    member val State: GatewayConnectionState = state
+
+    interface IDisposable with
+        member this.Dispose() =
+            this.Socket :> IDisposable |> _.Dispose()
+
+module GatewayConnection =
+    let onOpen (state: GatewayConnectionState) = fun () ->
+        ()
+
+    let onMessage (state: GatewayConnectionState) = fun (event: GatewayReceiveEvent) ->
+        ()
+
+    let onClose (state: GatewayConnectionState) = fun (code: GatewayCloseEventCode) ->
+        ()
+
+    // TODO: Implement lifecycle through above functions
+    // TODO: How to notify Gateway when GatewayConnection closes? TBD
+
+    let create gatewayUrl initialState =
+        let ws = new WebSocket(gatewayUrl)
+        let state = initialState |> Option.defaultValue (GatewayConnectionState.create ())
+
+        ws.OnOpen.Add(fun _ -> onOpen state ())
+
+        ws.OnMessage.Add(fun (e: MessageEventArgs) ->
+            e.Data
+            |> Decode.fromString GatewayReceiveEvent.decoder
+            |> Result.iter (onMessage state)
+        )
+
+        ws.OnClose.Add(fun e ->
+            e.Code
+            |> int
+            |> enum<GatewayCloseEventCode>
+            |> onClose state
+        )
+
+        new GatewayConnection(ws, state)
+
+    let connect (connection: GatewayConnection) =
+        connection.Socket.Connect()
+
+    let close (connection: GatewayConnection) =
+        connection.Socket.Close()
+
+    let send event (connection: GatewayConnection) =
+        match event with
+        | GatewaySendEvent.REQUEST_GUILD_MEMBERS d ->
+            Some {
+                Opcode = GatewayOpcode.REQUEST_GUILD_MEMBERS
+                Data = GatewaySendEventData.REQUEST_GUILD_MEMBERS d
+                Sequence = connection.State.Sequence
+                EventName = None
+            }
+
+        | _ ->
+            None
+
+        |> Option.iter (GatewaySendEventPayload.encoder >> Encode.toString 0 >> connection.Socket.Send)
