7210d678736201bc3686b6119e42e4c659ac9677
leydel <me@itsf.in>
2025-04-11 15:13:05 +1000

    Begin implementing gateway using WebSocketSharp package

---
+++ FSharp.Discord.Gateway/Gateway.fs
@@ -0,0 +1,59 @@
+﻿namespace FSharp.Discord.Gateway
+
+open FSharp.Discord.Types
+open FSharp.Discord.Types.Serialization
+open Thoth.Json.Net
+open WebSocketSharp
+
+type GatewayState = {
+    Sequence: int option
+    // TODO: Add other relevant lifecycle state here
+}
+
+type Gateway = {
+    Socket: WebSocket
+    State: GatewayState option
+}
+
+module Gateway =
+    let create gatewayUrl =
+        let ws = new WebSocket(gatewayUrl)
+
+        ws.OnOpen.Add(fun _ -> ()) // TODO: Handle initiating lifecycle
+        ws.OnMessage.Add(fun (e: MessageEventArgs) -> ()) // TODO: Handle lifecycle events then event handler
+        ws.OnError.Add(fun (e: ErrorEventArgs) -> ()) // TODO: Figure out when this occurs and what it should handle
+        ws.OnClose.Add(fun (e: CloseEventArgs) -> ()) // TODO: Handle resume/reconnect if appropriate (involves creating new ws ??)
+
+        ws
+
+    let private send event gateway =
+        gateway.State |> Option.iter (fun state ->
+            match event with
+            | GatewaySendEvent.REQUEST_GUILD_MEMBERS d ->
+                Some {
+                    Opcode = GatewayOpcode.REQUEST_GUILD_MEMBERS
+                    Data = GatewaySendEventData.REQUEST_GUILD_MEMBERS d
+                    Sequence = state.Sequence
+                    EventName = None
+                }
+
+            | _ ->
+                None
+
+            |> Option.iter (GatewaySendEventPayload.encoder >> Encode.toString 0 >> gateway.Socket.Send)
+        )
+
+    let requestGuildMembers event gateway =
+        send (GatewaySendEvent.REQUEST_GUILD_MEMBERS event) gateway
+
+    let requestSoundboardSounds event gateway =
+        send (GatewaySendEvent.REQUEST_SOUNDBOARD_SOUNDS event) gateway
+
+    let updateVoiceState event gateway =
+        send (GatewaySendEvent.UPDATE_VOICE_STATE event) gateway
+
+    let updatePresence event gateway =
+        send (GatewaySendEvent.UPDATE_PRESENCE event) gateway
+
+    let close (gateway: Gateway) =
+        gateway.Socket.Close()
