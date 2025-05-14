bd55bcc3102aeb5623a2975bd32fc3a079a07159
leydel <me@itsf.in>
2025-05-14 19:19:47 +1000

    Reorder nuget and gpr publishes and comment out broken gateway integration app project

--- Tests/FSharp.Discord.Gateway.Tests.Integration/Program.fs
+++ Tests/FSharp.Discord.Gateway.Tests.Integration/Program.fs
@@ -1,54 +1,58 @@
-﻿open FSharp.Discord.Gateway
-open FSharp.Discord.Gateway.Unsupported
-open FSharp.Discord.Rest
-open FSharp.Discord.Types
-open FsToolkit.ErrorHandling
-open System
-open System.Net.Http
-open System.Threading.Tasks
-open Thoth.Json.Net
+﻿module Program
 
-let token = "" // TODO: Turn into secret config value
+let todo = "implement this"
 
-asyncResult {
-    // Get gateway url from Discord
-    let! res, _ = Rest.getGateway (GetGatewayRequest("10", GatewayEncoding.JSON, None)) (new HttpClient())
-    let! gateway = res
-    let gatewayUrl = gateway.Url
+//open FSharp.Discord.Gateway
+//open FSharp.Discord.Gateway.Unsupported
+//open FSharp.Discord.Rest
+//open FSharp.Discord.Types
+//open FsToolkit.ErrorHandling
+//open System
+//open System.Net.Http
+//open System.Threading.Tasks
+//open Thoth.Json.Net
 
-    // Create identify event
-    let identify = {
-        Token = token
-        Intents = GatewayIntent.ALL
-        Shard = None
-        Properties = {
-            OperatingSystem = "OperatingSystem"
-            Browser = "Browser"
-            Device = "Device"
-        }
-        Compress = None
-        LargeThreshold = None
-        Presence = None
-    }
+//let token = "" // TODO: Turn into secret config value
 
-    // Create dispatcher
-    let dispatcher (event: GatewayReceiveEvent) =
-        let json = Encode.toString 0 (GatewayReceiveEvent.encoder event)
-        Console.WriteLine json
+//asyncResult {
+//    // Get gateway url from Discord
+//    let! res, _ = Rest.getGateway (GetGatewayRequest("10", GatewayEncoding.JSON, None)) (new HttpClient())
+//    let! gateway = res
+//    let gatewayUrl = gateway.Url
 
-    // Create gateway connection
-    Gateway.create gatewayUrl identify dispatcher
-    |> Gateway.connect
-    |> ignore
+//    // Create identify event
+//    let identify = {
+//        Token = token
+//        Intents = GatewayIntent.ALL
+//        Shard = None
+//        Properties = {
+//            OperatingSystem = "OperatingSystem"
+//            Browser = "Browser"
+//            Device = "Device"
+//        }
+//        Compress = None
+//        LargeThreshold = None
+//        Presence = None
+//    }
 
-    do! Task.Delay (1000 * 60 * 60) // arbitrary
-}
-|> AsyncResult.mapError (fun e ->
-    Console.WriteLine $"Error: {e}"
-    e
-)
-|> Async.Ignore
-|> Async.RunSynchronously
+//    // Create dispatcher
+//    let dispatcher (event: GatewayReceiveEvent) =
+//        let json = Encode.toString 0 (GatewayReceiveEvent.encoder event)
+//        Console.WriteLine json
 
-// TODO: Rewrite gateway connection to use FSharp.Control.Websockets since WebSocketSharp only supports .NET Framework...
-// At least it will be more functional and won't have the whole ugly event emitter and stuff above
+//    // Create gateway connection
+//    Gateway.create gatewayUrl identify dispatcher
+//    |> Gateway.connect
+//    |> ignore
+
+//    do! Task.Delay (1000 * 60 * 60) // arbitrary
+//}
+//|> AsyncResult.mapError (fun e ->
+//    Console.WriteLine $"Error: {e}"
+//    e
+//)
+//|> Async.Ignore
+//|> Async.RunSynchronously
+
+//// TODO: Rewrite gateway connection to use FSharp.Control.Websockets since WebSocketSharp only supports .NET Framework...
+//// At least it will be more functional and won't have the whole ugly event emitter and stuff above
