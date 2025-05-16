945f21043cad16a2a988c0af0ba794f0bdf902ff
leymbda <me@itsf.in>
2025-05-16 15:08:49 +1000

    Create wrapper for async msg handling

---
+++ FSharp.Discord.Gateway/AsyncMsg.fs
@@ -0,0 +1,32 @@
+﻿namespace FSharp.Discord.Gateway
+
+open Elmish
+
+[<RequireQualifiedAccess>]
+type AsyncAttemptMsg<'Input> =
+    | Attempt of 'Input
+    | Failure of exn
+
+module AsyncAttemptMsg =
+    let inline toCmd (task: 'Args -> Async<'Success>) (args: 'Args) (msg: AsyncAttemptMsg<'Args> -> 'Msg) =
+        Cmd.OfAsync.attempt task args (AsyncAttemptMsg.Failure >> msg)
+
+[<RequireQualifiedAccess>]
+type AsyncEitherMsg<'Input, 'Success> =
+    | Either of 'Input
+    | Success of 'Success
+    | Failure of exn
+
+module AsyncEitherMsg =
+    let inline toCmd (task: 'Args -> Async<'Success>) (args: 'Args) (msg: AsyncEitherMsg<'Args, 'Success> -> 'Msg) =
+        Cmd.OfAsync.either task args (AsyncEitherMsg.Success >> msg) (AsyncEitherMsg.Failure >> msg)
+           
+[<RequireQualifiedAccess>]
+type AsyncPerformMsg<'Input, 'Success> =
+    | Perform of 'Input
+    | Success of 'Success
+
+module AsyncPerformMsg =
+    let inline toCmd (task: 'Args -> Async<'Success>) (args: 'Args) (msg: AsyncPerformMsg<'Args, 'Success> -> 'Msg) =
+        Cmd.OfAsync.perform task args (AsyncPerformMsg.Success >> msg)
+ 
--- FSharp.Discord.Gateway/Gateway.fs
+++ FSharp.Discord.Gateway/Gateway.fs
@@ -1,18 +1,31 @@
 ﻿module FSharp.Discord.Gateway.Gateway
 
 open Elmish
+open FSharp.Control.Websockets
 open FSharp.Discord.Gateway
 open FSharp.Discord.Types
 open System
+open System.Net.WebSockets
+open System.Threading
+
+type Handler = GatewayReceiveEvent -> Async<unit>
 
 type Model = {
+    GatewayUri: Uri
+    Identify: IdentifySendEvent
+    Handler: Handler
+    Socket: ThreadSafeWebSocket.ThreadSafeWebSocket option
     Heartbeat: Heartbeat.Model option
     Lifecycle: Lifecycle.Model option
 }
 
 module Model =
-    let zero () =
+    let zero gatewayUri identify handler =
         {
+            GatewayUri = gatewayUri
+            Identify = identify
+            Handler = handler
+            Socket = None
             Lifecycle = None
             Heartbeat = None
         }
@@ -21,30 +34,59 @@ type Msg =
     | Heartbeat of Heartbeat.Msg
     | Lifecycle of Lifecycle.Msg
 
-    | Connect of IdentifySendEvent
-    | Reconnect
-    | Disconnect
+    | Connect of AsyncEitherMsg<Uri option, ThreadSafeWebSocket.ThreadSafeWebSocket>
+    | Reconnect of AsyncPerformMsg<Uri option, ThreadSafeWebSocket.ThreadSafeWebSocket>
+    | Disconnect of AsyncPerformMsg<unit, unit>
 
     // TODO: Handle websocket action success/failure
     // TODO: Handle sending and receiving gateway events
+    // TODO: Msg to detect for termination
 
-let init () =
-    Model.zero (), Cmd.none
+let private connect model resumeUri = async {
+    let uri = resumeUri |> Option.defaultValue model.GatewayUri
+
+    let ws = new ClientWebSocket() // TODO: Partially apply to allow testing with a mock socket
+    do! ws.ConnectAsync(uri, CancellationToken.None) |> Async.AwaitTask
+    return ThreadSafeWebSocket.createFromWebSocket ws
+}
+
+let init (gatewayUri, identify, handler) =
+    Model.zero gatewayUri identify handler, Cmd.none
 
 let update msg model =
     match model.Lifecycle, model.Heartbeat, msg with
     // Connect
-    | None, None, Msg.Connect identify ->
-        let lifecycle, cmd = Lifecycle.init identify
+    | None, None, Msg.Connect (AsyncEitherMsg.Either resumeUri) ->
+        let lifecycle, cmd = Lifecycle.init model.Identify
 
-        { model with Lifecycle = Some lifecycle }, Cmd.map Msg.Lifecycle cmd
+        { model with Lifecycle = Some lifecycle },
+        Cmd.batch [
+            Cmd.map Msg.Lifecycle cmd
+            AsyncEitherMsg.toCmd (connect model) resumeUri Msg.Connect
+        ]
 
-    // Reconnect
-    | _, _, Msg.Reconnect ->
+    | None, None, Msg.Connect (AsyncEitherMsg.Success socket) ->
         model, Cmd.none // TODO: Implement
 
+    | None, None, Msg.Connect (AsyncEitherMsg.Failure exn) ->
+        eprintfn "%A" exn
+        model, Cmd.none // TODO: Implement
+
+    // Reconnect
+    | _, _, Msg.Reconnect (AsyncPerformMsg.Perform resumeUri) ->
+        model, Cmd.none // TODO: Trigger disconnect then reconnect
+        
+    | _, _, Msg.Reconnect (AsyncPerformMsg.Success socket) ->
+        model, Cmd.none // TODO: Implement
+
+    // TODO: Reconnect should instead call disconnect then connect...
+    // TODO: If above, disconnect needs to know whether to or not to terminate
+
     // Disconnect
-    | _, _, Msg.Disconnect ->
+    | _, _, Msg.Disconnect (AsyncPerformMsg.Perform _) ->
+        model, Cmd.none // TODO: Implement
+        
+    | _, _, Msg.Disconnect (AsyncPerformMsg.Success _) ->
         model, Cmd.none // TODO: Implement
 
     // Lifecycle hello start heartbeat
@@ -87,7 +129,7 @@ let update msg model =
         { model with Lifecycle = Some updated },
         Cmd.batch [
             Cmd.map Msg.Lifecycle cmd
-            Cmd.ofMsg Msg.Disconnect
+            Cmd.ofMsg (Msg.Disconnect (AsyncPerformMsg.Perform ()))
         ]
 
     // Catch remaining lifecycle messages
@@ -103,7 +145,7 @@ let update msg model =
         { model with Heartbeat = Some updated },
         Cmd.batch [
             Cmd.map Msg.Heartbeat cmd
-            Cmd.ofMsg Msg.Disconnect
+            Cmd.ofMsg (Msg.Disconnect (AsyncPerformMsg.Perform ()))
         ]
 
         // TODO: Handle heartbeat stop
@@ -117,7 +159,7 @@ let update msg model =
     // Catch invalid messages
     | lifecycle, heartbeat, msg ->
         eprintfn "Attempted to call msg %A in invate state %A %A" msg lifecycle heartbeat
-        model, Cmd.ofMsg Msg.Disconnect
+        model, Cmd.ofMsg (Msg.Disconnect (AsyncPerformMsg.Perform ()))
 
 let subscribe model: Sub<Msg> =
     [] // TODO: Implement all necessary subscriptions
